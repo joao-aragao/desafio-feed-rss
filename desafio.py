b7dd1223d86e652bd346c366ff855d8acb22e13f
Joao Aragao <joaovictoraragaoads@gmail.com>
2021-02-27 12:05:04 -0300

    Comentando código

--- desafio.py
+++ desafio.py
@@ -4,26 +4,45 @@ import json
 from bs4 import BeautifulSoup
 
 
+# Cria uma função para receber a url do xml
 def getUrl(url):
+    # Fazendo request
     resp = requests.get(url)
 
+    # Verificar request
+    if resp.status_code == 200:
+        print('Ok')
+    else:
+        print('Error')
+        exit()
+
+    # Chamando construtor
     soup = BeautifulSoup(resp.content, features='xml')
 
+    # Procurando tag tem e atribuindo a variável
     itens = soup.findAll("item")
 
+    # crio lista para receber tudo que achou de item
     lista_itens = []
 
+    # crio for para listar item dentro de itens:
     for item in itens:
+        # Setto um dicionário para adicionar os itens exibidos a lista_itens
         dic_item = {}
+        # Pegando link
         dic_item['site:'] = item.link.text
-        dic_item['data:'] = item.pubDate.text[1].split()
+        # Pegando Data de publicação
+        dic_item['data:'] = item.pubDate.text
+        # Adicionando itens a lista
         lista_itens.append(dic_item)
 
+    # Função json.dumps para transformar lista em formato json
     lista_itens = json.dumps(lista_itens)
 
-    return print(lista_itens)
+    print(lista_itens)
 
 
-# getUrl('https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss')
+# Chamando função getUrl
+getUrl('https://www.portugal-a-programar.pt/forums/topic/11371-inverter-ordem-em-string/')
 # getUrl('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml')
 # getUrl('https://g1.globo.com/rss/g1/carros/')
