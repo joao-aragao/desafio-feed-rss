321fbd2d73e01ad0d2fd9483fd1364a85dbb9ddb
Joao Aragao <joaovictoraragaoads@gmail.com>
2021-02-27 12:00:13 -0300

    Finish Script

--- desafio.py
+++ desafio.py
@@ -1,4 +1,6 @@
+# Lib necessárias para rodar script
 import requests
+import json
 from bs4 import BeautifulSoup
 
 
@@ -7,7 +9,21 @@ def getUrl(url):
 
     soup = BeautifulSoup(resp.content, features='xml')
 
-    return print(soup.prettify())
+    itens = soup.findAll("item")
+
+    lista_itens = []
+
+    for item in itens:
+        dic_item = {}
+        dic_item['site:'] = item.link.text
+        dic_item['data:'] = item.pubDate.text[1].split()
+        lista_itens.append(dic_item)
+
+    lista_itens = json.dumps(lista_itens)
+
+    return print(lista_itens)
 
 
-getUrl('https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss')
+# getUrl('https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss')
+# getUrl('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml')
+# getUrl('https://g1.globo.com/rss/g1/carros/')
